59cdde8c9da482bba027736547643b0ea2b0ab84
maxieo <97198812+maxieo@users.noreply.github.com>
2022-06-15 11:04:57 +0000

    Deploying to gh-pages from @ maxieo/NovaProspect-htmlcss@30700a196fa3f6dfc1ae58a10d7a37284a99b980 🚀

---
+++ index.d7a77d4e.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("visually-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.d7a77d4e.js.map
