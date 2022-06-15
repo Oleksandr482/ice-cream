2b7e9aa5d829d5b838e2fc78931ec874719bf6e8
maxieo <maxim.ostrovskyw@gmail.com>
2022-06-15 15:49:55 +0300

    Update scroll.js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,11 +1,17 @@
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
+const refs = {
+    body: document.querySelector('body'),
+    menu: document.querySelector('.mob-menu'),
+    smoothLinks: document.querySelectorAll('a[href^="#"]'),
+  };
+  for (let smoothLink of refs.smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+      e.preventDefault();
+      const id = smoothLink.getAttribute('href');
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      refs.body.classList.toggle('no-scroll');
+      refs.menu.classList.toggle('visually-hidden');
     });
-};
+  }
